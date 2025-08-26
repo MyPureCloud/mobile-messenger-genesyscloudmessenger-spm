1dad41c64b1438ae7e10106fbde87de6b733cc0c
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2025-08-26 11:47:40 +0000

    [Update] 1.16.0

--- Package.swift
+++ Package.swift
@@ -17,19 +17,19 @@ let package = Package(
         .package(
             name: "GenesysCloudCore",
             url: "https://github.com/MyPureCloud/mobile-messenger-genesyscloudcore-spm.git",
-            .exact("1.15.0")
+            .exact("1.16.0")
         ),
         .package(
             name: "GenesysCloudMessengerTransport",
             url: "https://github.com/MyPureCloud/mm-genesyscloudmessengertransport-spm.git",
-            .exact("2.8.7")
+            .exact("2.9.1")
         )
     ],
     targets: [
         .binaryTarget(
             name: "GenesysCloudMessenger",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudMessenger/GenesysCloudMessenger_version_t1.15.0_commit_a92c3d5d2dc04dd76e1a23bd4137778b74a76c96.zip",
-            checksum: "3573219cbf7a39fa7c45912140bf9724e33ca1df3651733db1029133b8814d87"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.prod/GenesysCloudMessenger/GenesysCloudMessenger_version_t1.16.0_commit_48206b53fc3ca4beef9691ec9177079ae15bd7be.zip",
+            checksum: "96eb0ed803dbeee6c538f6531b5f979c678bbf2040c48a159b52fc4280530001"
         ),
         .target(
             name: "GenesysCloudMessengerWrapper",
